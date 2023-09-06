ce9881a5b446ccfc6f055abf6f978a022128c77f
TomasGonzz00 <tomasgonzalez4278@gmail.com>
2023-09-06 13:49:14 -0300

    productos mock

---
+++ src/mocks/products_mock.js
@@ -0,0 +1,17 @@
+export const products_mock = [
+    {
+        categoria: "Tarjestas & Dijes"
+    },
+    {
+        categoria: "Dijes de Mesa"
+    },
+    {
+        categoria: "Regalos & Recuerdos"
+    },
+    {
+        categoria: "Toppers 3D"
+    },
+    {
+        categoria: "Regalos institucionales"
+    },
+]
