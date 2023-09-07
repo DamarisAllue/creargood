f178a7ccee1f29e43806dfea25336842249bd178
Damaris Allue <116525982+DamarisAllue@users.noreply.github.com>
2023-09-07 16:26:58 -0300

    empezando quienes somos

--- src/components/quienessomos.js
+++ src/components/quienessomos.js
@@ -3,11 +3,14 @@ import banner from "../assets/imagenHERORosa.png"
 
 const quienessomos = () => {
     const titulo = "¿Quiénes somos?";
-    return ( 
+    return (
         <Layout banner={banner} titulo={titulo}>
 
+            <div className='corchetes-contacto'>
+                <h1 className='titulo-contacto'>Somos un emprendimiento con talento humano creativo, que escuchamos y creamos juntos a nuestros clientes ideas originales, que se queden en el recuerdo de los usuarios.<br />Entendemos a nuestros clientes, desde el poco tiempo que tiene para encargarse de producir un souvenir o  regalos para eventos sociales.<br />Por ello nuestra forma de trabajo es acompañar y empoderarlos a generar eseproducto original.</h1>
+            </div>
         </Layout>
-     );
+    );
 }
- 
+
 export default quienessomos;
