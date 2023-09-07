47faea16e185d850b8f8d44cb575f527035d2ced
TomasGonzz00 <tomasgonzalez4278@gmail.com>
2023-09-07 12:42:48 -0300

    home productos casi lista

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import { Layout } from "./components/Layout";
 import banner from "./assets/imagenHERO.png";
 import { home_mock } from "./mocks/home_mock";
 import HomeItem from "./components/HomeItem";
+import { products_mock } from "./mocks/products_mock";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { useState } from "react";
 
 function App() {
+  const [slides, setSlides] = useState(products_mock);
   return (
     <Layout banner={banner}>
       <div className="home">
@@ -18,7 +27,38 @@ function App() {
             Diseños creativos a medida de tus necesidades
           </p>
         </div>
-        {home_mock.map((data, index) => <HomeItem item={data} index={index}/>)}
+        {home_mock.map((data, index) => <HomeItem item={data} index={index} />)}
+        <div className="home-products-container">
+          <p className="home-products-title">PRODUCTOS</p>
+          <div className="home-products">
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              navigation={true} //botones 
+              grabCursor={true}
+              loop={true}
+              breakpoints={{
+                900: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                1250: {
+                  slidesPerView: 4,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Navigation]}
+              className="mySwiper"
+            >
+              {products_mock.map(data => (
+                <SwiperSlide>
+                  <img src={data[0].img} />
+                </SwiperSlide>))
+              }
+            </Swiper>
+          </div>
+          <button className="home-more-btn">VER MÁS</button>
+        </div>
       </div>
     </Layout>
   );
